0fee9028ef7622bf41cfb51d27d05560134ce4ae
sirisneha99 <nsnehasiri@gmail.com>
2025-09-25 11:27:51 -0400

    Initial Commit

---
+++ Scavenger Hunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scavenger Hunt
+//
+//  Created by sneha siri nagabathula on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scavenger Hunt/Scavenger_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scavenger_HuntApp.swift
+//  Scavenger Hunt
+//
+//  Created by sneha siri nagabathula on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Scavenger_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
